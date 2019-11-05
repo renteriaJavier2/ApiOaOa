472b1c64e665119d0f9f8f6e8d0db460cd5906bf
renteriaJavier2 <gitrepouse@gmail.com>
2019-11-05 04:23:36 +0000

    Start to beginning

--- models/conctactstypes.js
+++ models/conctactstypes.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const ConctactsTYpes = sequelize.define('ConctactsTYpes', {
     name: DataTypes.STRING
-  }, {});
+  }, {}); 
   ConctactsTYpes.associate = function(models) {
     // associations can be defined here
   };
